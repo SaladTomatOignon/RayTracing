666a62548ba6cdfc46d485fe38b38187078fd94b
Samy WADAN <SaladeTomatOignon@hotmail.com>
2020-05-17 20:12:47 +0200

    Revert "Ajout des objets Matrice"
    This reverts commit c23522957aa323a13860d7f4bc21242904e5e05c.

--- include/scene/forme.h
+++ include/scene/forme.h
@@ -1,18 +1,12 @@
 #ifndef DEF__FORME
 #define DEF__FORME
 
-#include "../geometrie/matrice.h"
-
 class Forme {
     public:
         Forme();
         ~Forme();
 
         virtual Forme* clone() const = 0;
-
-    protected:
-        Matrice Md = Matrice(4, 4);
-        Matrice Mi = Matrice(4, 4);
 };
 
 #endif
