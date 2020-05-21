032954613b313c904fdb2fdd24d080793249eebb
Samy WADAN <SaladeTomatOignon@hotmail.com>
2020-05-21 21:31:40 +0200

    Acceptation d'un vecteur unitaire uniquement en argument

--- src/scene/camera.cpp
+++ src/scene/camera.cpp
@@ -1,6 +1,7 @@
 #include "../../include/scene/camera.h"
 #include "../../include/geometrie/point.h"
 #include "../../include/geometrie/vecteur.h"
+#include <stdexcept>
 
 Camera::Camera() : Camera(Point(), Vecteur()) {
 
@@ -11,6 +12,10 @@ Camera::Camera(const Camera& camera) : Camera(Point(camera.position), Vecteur(ca
 }
 
 Camera::Camera(Point position, Vecteur orientation) {
+    if (orientation != orientation.unitaire()) { /* Si le vecteur donné n'est pas unitaire */
+        throw std::invalid_argument("Le vecteur d'orientation de la caméra doit être un vecteur unitaire");
+    }
+
     this->position = Point(position);
     this->orientation = Vecteur(orientation);
 }
