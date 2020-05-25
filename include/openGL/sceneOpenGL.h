c8bcb07159fffe3401368ed47382227d9f0bd99d
Samy WADAN <SaladeTomatOignon@hotmail.com>
2020-05-25 14:38:54 +0200

    Ajout d'un objet Input dans la scène openGL pour gérer les interactions. Ajoute la possibilité de se déplacer dans la scène

--- include/openGL/sceneOpenGL.h
+++ include/openGL/sceneOpenGL.h
@@ -8,13 +8,17 @@
     #include <GL3/gl3.h>
 #endif
 
+#include "../scene/scene.h"
+#include "../outils/image.h"
+#include "../openGL/input.h"
+
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <string>
 
 class SceneOpenGL {
     public:
-        SceneOpenGL(std::string titreFenetre, int largeurFenetre, int hauteurFenetre);
+        SceneOpenGL(std::string titreFenetre, int largeurFenetre, int hauteurFenetre, Scene scene);
         ~SceneOpenGL();
 
         bool initialiserFenetre();
@@ -29,6 +33,15 @@ class SceneOpenGL {
         SDL_Window* m_fenetre;
         SDL_GLContext m_contexteOpenGL;
         SDL_Event m_evenements;
+
+        // Objet Input pour la gestion des évènements
+        Input m_input;
+
+        Scene m_scene;
+
+        void afficherImage(Image& image);
+        void deplacerCamera();
+        void orienterCamera();
 };
 
 #endif
--- src/openGL/sceneOpenGL.cpp
+++ src/openGL/sceneOpenGL.cpp
@@ -1,4 +1,8 @@
 #include "../../include/openGL/sceneOpenGL.h"
+#include "../../include/openGL/input.h"
+#include "../../include/outils/image.h"
+#include "../../include/scene/scene.h"
+#include "../../include/application.h"
 
 #include <SDL2/SDL.h>
 #include <iostream>
@@ -6,12 +10,14 @@
 
 using namespace std;
 
-SceneOpenGL::SceneOpenGL(std::string titreFenetre, int largeurFenetre, int hauteurFenetre) {
+SceneOpenGL::SceneOpenGL(string titreFenetre, int largeurFenetre, int hauteurFenetre, Scene scene) {
     m_titreFenetre = titreFenetre;
     m_largeurFenetre = largeurFenetre;
     m_hauteurFenetre = hauteurFenetre;
     m_fenetre = nullptr;
     m_contexteOpenGL = nullptr;
+    m_scene = Scene(scene);
+    m_input = Input();
 }
 
 SceneOpenGL::~SceneOpenGL() {
@@ -91,21 +97,117 @@ bool SceneOpenGL::initGL() {
 }
 
 void SceneOpenGL::bouclePrincipale() {
-    // Variables
+    Image rendu(m_scene.grille.resolution_l, m_scene.grille.resolution_h);
+
+    // Variables relatives à la boucle
     bool terminer(false);
+    unsigned int frameRate(1000 / 50);
+    Uint32 debutBoucle(0), finBoucle(0), tempsEcoule(0);
 
     // Boucle principale
-    while (!terminer) {
-        // Gestion des évènements
-        SDL_WaitEvent(&m_evenements);
+    while (!m_input.terminer()) {
+        // On définit le temps de début de boucle
+        debutBoucle = SDL_GetTicks();
 
-        if (m_evenements.window.event == SDL_WINDOWEVENT_CLOSE)
-            terminer = true;
+        /* --------------------------------------------------------------- */
+        /* ---------------------- GESTION DE LA LOGIQUE ------------------ */
+        /* --------------------------------------------------------------- */
+
+        Application::lancerRayons(m_scene, rendu);
+
+        // Gestion des évènements
+        m_input.updateEvenements();
+
+        if (m_input.getTouche(SDL_SCANCODE_ESCAPE))
+            break;
+
+        deplacerCamera();
+        orienterCamera();
+
+        /* --------------------------------------------------------------- */
+        /* ---------------------- AFFICHAGE DE L'ECRAN ------------------- */
+        /* --------------------------------------------------------------- */
 
         // Nettoyage de l'écran
         glClear(GL_COLOR_BUFFER_BIT);
 
+        afficherImage(rendu);
+
         // Actualisation de la fenêtre
         SDL_GL_SwapWindow(m_fenetre);
+
+        // Calcul du temps écoulé
+        finBoucle = SDL_GetTicks();
+        tempsEcoule = finBoucle - debutBoucle;
+
+        // Si nécessaire, on met en pause le programme
+        if (tempsEcoule < frameRate)
+            SDL_Delay(frameRate - tempsEcoule);
+    }
+
+}
+
+void SceneOpenGL::afficherImage(Image& image) {
+    int nbPixels = image.largeur * image.hauteur * 3;
+    unsigned char* window_RGBData = new unsigned char[nbPixels]();
+
+    for (unsigned int i = 0; i < image.hauteur; i++) {
+        for (unsigned int j = 0; j < image.largeur; j++) {
+            window_RGBData[3 * (i * image.hauteur + j) + 0] = image[image.hauteur - 1 - i][j].r;
+            window_RGBData[3 * (i * image.hauteur + j) + 1] = image[image.hauteur - 1 - i][j].g;
+            window_RGBData[3 * (i * image.hauteur + j) + 2] = image[image.hauteur - 1 - i][j].b;
+        }
+    }
+
+    glDrawPixels(300, 300, GL_RGB, GL_UNSIGNED_BYTE, window_RGBData);
+
+    delete [] window_RGBData;
+}
+
+void SceneOpenGL::deplacerCamera() {
+    bool deplacer = false;
+
+    /* ATTENTION : SDL ne considère que les clavier QWERTY */
+
+    if (m_input.getTouche(SDL_SCANCODE_A)) {
+        m_scene.camera.translater(Camera::DIRECTION::GAUCHE);
+        deplacer = true;
+    }
+
+    if (m_input.getTouche(SDL_SCANCODE_W)) {
+        m_scene.camera.translater(Camera::DIRECTION::DEVANT);
+        deplacer = true;
+    }
+
+    if (m_input.getTouche(SDL_SCANCODE_D)) {
+        m_scene.camera.translater(Camera::DIRECTION::DROITE);
+        deplacer = true;
+    }
+
+    if (m_input.getTouche(SDL_SCANCODE_S)) {
+        m_scene.camera.translater(Camera::DIRECTION::DERRIERE);
+        deplacer = true;
+    }
+
+    if (deplacer) {
+        m_scene.grille.positionner(m_scene.camera);
+    }
+}
+
+void SceneOpenGL::orienterCamera() {
+    if (m_input.getTouche(SDL_SCANCODE_LEFT)) {
+        m_scene.camera.orienter(Camera::DIRECTION::GAUCHE);
+    }
+
+    if (m_input.getTouche(SDL_SCANCODE_UP)) {
+        m_scene.camera.orienter(Camera::DIRECTION::HAUT);
+    }
+
+    if (m_input.getTouche(SDL_SCANCODE_RIGHT)) {
+        m_scene.camera.orienter(Camera::DIRECTION::DROITE);
+    }
+
+    if (m_input.getTouche(SDL_SCANCODE_DOWN)) {
+        m_scene.camera.orienter(Camera::DIRECTION::BAS);
     }
 }
